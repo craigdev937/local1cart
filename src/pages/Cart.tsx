d1b96f20da88470736115121a3d336492a9f4a39
Craig Johnson <craigdev937@gmail.com>
2024-09-05 15:33:47 -0400

    Created Cart, Shop, and NotFound pages.  I also updated the CSS.

---
+++ src/pages/Cart.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Cart = () => {
+    return (
+        <React.Fragment>
+            <h1>Cart Page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ducimus odit praesentium consequuntur. Cum et maiores expedita iusto, dolorum unde eaque fuga, numquam ipsum vel aut, exercitationem sapiente. Dolor, vitae.</p>
+        </React.Fragment>
+    );
+};
+
+
---
+++ src/pages/Shop.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Shop = () => {
+    return (
+        <React.Fragment>
+            <h1>Shop Page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ducimus odit praesentium consequuntur. Cum et maiores expedita iusto, dolorum unde eaque fuga, numquam ipsum vel aut, exercitationem sapiente. Dolor, vitae.</p>
+        </React.Fragment>
+    );
+};
+
+
